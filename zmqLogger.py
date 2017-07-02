63bd375662881671275b7e66ce9508c6b4e475e1
TeaPackCZ <honza.crha@gmail.com>
2017-07-03 00:20:42 +0200

    Correct bad design of PUB/SUB mechanism  - PUB can emit on multiple ports, but on each port, there can be only one SUB
    Signed-off-by: TeaPackCZ <honza.crha@gmail.com>

--- zmqLogger.py
+++ zmqLogger.py
@@ -2,11 +2,8 @@ from Logger import zMQLogger
 from time import sleep
 
 A = zMQLogger()
-A.addLocalPort("10101") # GPS1
-A.addLocalPort("10102") # GPS2
-A.addLocalPort("10301") # test1
-A.addLocalPort("10302") # test2
-A.addIP("192.168.0.1:9000") # RaspiCam?
+A.addLocalPort("10000")
+A.addIP("192.168.0.1:10000") # RaspiCam?
 
 sleep(1)
 
