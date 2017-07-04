435c85dfe59ed95a489caaa29a019d3e1ba54538
TeaPackCZ <honza.crha@gmail.com>
2017-07-04 21:15:20 +0200

    Include gpsNAV to Logger and init script
    Signed-off-by: TeaPackCZ <honza.crha@gmail.com>

--- master_init.sh
+++ master_init.sh
@@ -2,6 +2,8 @@
 
 python zmqLogger.py &
 
+python gpsNavigation.py &
+
 #python gpsReader.py /dev/ttyS3 38400 10101 GPS1 &
 python gpsReader.py Logs/log_GPS1_15_45_36 38400 10101 GPS1 &
 python gpsReader.py Logs/log_GPS2_15_45_36 38400 10102 GPS2 &
--- zmqLogger.py
+++ zmqLogger.py
@@ -4,6 +4,7 @@ from time import sleep
 A = zMQLogger()
 A.addLocalPort("10101") # GPS1
 A.addLocalPort("10102") # GPS2
+A.addLocalPort("10111") # GPSNav
 A.addLocalPort("10301") # test1
 A.addLocalPort("10302") # test2
 A.addIP("192.168.0.1:9000") # RaspiCam?
