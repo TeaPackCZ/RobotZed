a67917c46ba0496f0d0003437471338894cc7277
TeaPackCZ <honza.crha@gmail.com>
2017-07-06 19:05:02 +0200

    GPS chips now sends data on 10Hz and on 115200 baud
    Signed-off-by: TeaPackCZ <honza.crha@gmail.com>

--- master_init.sh
+++ master_init.sh
@@ -5,8 +5,10 @@ python zmqLogger.py &
 python main.py &
 python gpsNavigation.py &
 
-#python gpsReader.py /dev/ttyS3 38400 10101 GPS1 &
-python gpsReader.py Logs/log_GPS1_15_45_36 38400 10101 GPS1 &
-python gpsReader.py Logs/log_GPS2_15_45_36 38400 10102 GPS2 &
+#python gpsReader.py /dev/ttyS3 115200 10101 GPS1 &
+#python gpsReader.py /dev/ttyS4 115200 10102 GPS2 &
+
+python gpsReader.py Logs/log_GPS1_15_45_36 115200 10101 GPS1 &
+python gpsReader.py Logs/log_GPS2_15_45_36 115200 10102 GPS2 &
 
 exit 0
