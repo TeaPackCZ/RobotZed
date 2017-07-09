4f92c5b92022d7186dbadf7cc60b33c532ccb4e1
TeaPackCZ <honza.crha@gmail.com>
2017-07-09 08:48:16 +0200

    Add test for navigational functions - Distance and Azimuth
    Signed-off-by: TeaPackCZ <honza.crha@gmail.com>

--- SelfTests.py
+++ SelfTests.py
@@ -33,6 +33,53 @@ class TestLogger(unittest.TestCase):
         ## cleanup
         os.remove(testLog.name)
 
+from gpsNavigation import gpsModule
+class TestGPSNavigation(unittest.TestCase):
+    def test_gps_angles(self):
+        gpsMod = gpsModule()
+        gpsMod.updateData("ID:GPS1;LAT:10,N;LON:10,E;P_ERR:1.0;TIME:1000")
+        gpsMod.updateData("ID:GPS2;LAT:10,N;LON:10,E;P_ERR:1.0;TIME:1000")
+
+        gpsMod.updateWayPoint("ID:MAIN;LON:10.1;LAT:10.1;ERR:3")
+        distance, azimut = gpsMod.GPSData.getDirAndDist(gpsMod.WayPoint)
+        self.assertEqual(distance,15623.0)
+        self.assertEqual(azimut,45.0)
+
+        gpsMod.updateWayPoint("ID:MAIN;LON:10.0;LAT:10.1;ERR:3")
+        distance, azimut = gpsMod.GPSData.getDirAndDist(gpsMod.WayPoint)
+        self.assertEqual(distance,10963.0)
+        self.assertEqual(azimut,90.0)
+
+        gpsMod.updateWayPoint("ID:MAIN;LON:9.9;LAT:10.1;ERR:3")
+        distance, azimut = gpsMod.GPSData.getDirAndDist(gpsMod.WayPoint)
+        self.assertEqual(distance,15625.0)
+        self.assertEqual(azimut,135.0)
+
+        gpsMod.updateWayPoint("ID:MAIN;LON:9.9;LAT:10.0;ERR:3")
+        distance, azimut = gpsMod.GPSData.getDirAndDist(gpsMod.WayPoint)
+        self.assertEqual(distance,11132.0)
+        self.assertEqual(azimut,180.0)
+
+        gpsMod.updateWayPoint("ID:MAIN;LON:9.9;LAT:9.9;ERR:3")
+        distance, azimut = gpsMod.GPSData.getDirAndDist(gpsMod.WayPoint)
+        self.assertEqual(distance,15625.0)
+        self.assertEqual(azimut,225.0)
+
+        gpsMod.updateWayPoint("ID:MAIN;LON:10.0;LAT:9.9;ERR:3")
+        distance, azimut = gpsMod.GPSData.getDirAndDist(gpsMod.WayPoint)
+        self.assertEqual(distance,10963.0)
+        self.assertEqual(azimut,270.0)
+
+        gpsMod.updateWayPoint("ID:MAIN;LON:10.1;LAT:9.9;ERR:3")
+        distance, azimut = gpsMod.GPSData.getDirAndDist(gpsMod.WayPoint)
+        self.assertEqual(distance,15623.0)
+        self.assertEqual(azimut,315.0)
+
+        gpsMod.updateWayPoint("ID:MAIN;LON:10.1;LAT:10;ERR:3")
+        distance, azimut = gpsMod.GPSData.getDirAndDist(gpsMod.WayPoint)
+        self.assertEqual(distance,11132.0)
+        self.assertEqual(azimut,0)
+
 
 if __name__ == '__main__':
     unittest.main()
