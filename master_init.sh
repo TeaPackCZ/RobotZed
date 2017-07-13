8d1e75b36b70bdcef91439cb2de55c6a2b2d36a3
TeaPackCZ <honza.crha@gmail.com>
2017-07-14 00:39:43 +0200

    Switch init to run on real HW
    Signed-off-by: TeaPackCZ <honza.crha@gmail.com>

--- master_init.sh
+++ master_init.sh
@@ -5,10 +5,12 @@ python zmqLogger.py &
 python main.py &
 python gpsNavStarter.py &
 
-#python gpsReader.py /dev/ttyS3 115200 10101 GPS1 &
-#python gpsReader.py /dev/ttyS4 115200 10102 GPS2 &
+python mbed.py /dev/ttyS2 115200 10201 mbed &
 
-python gpsReader.py Logs/log_GPS1_15_45_36 115200 10101 GPS1 &
-python gpsReader.py Logs/log_GPS2_15_45_36 115200 10102 GPS2 &
+python gpsReader.py /dev/ttyS3 115200 10101 GPS1 &
+python gpsReader.py /dev/ttyS4 115200 10102 GPS2 &
+
+#python gpsReader.py Logs/log_GPS1_15_45_36 115200 10101 GPS1 &
+#python gpsReader.py Logs/log_GPS2_15_45_36 115200 10102 GPS2 &
 
 exit 0
