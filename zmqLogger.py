99e5e39d05af1a4cde3cfac8b8af3f4268111c97
TeaPackCZ <honza.crha@gmail.com>
2017-07-17 07:05:14 +0200

    Add mbeds ZMQ ports to zmqLogger
    Signed-off-by: TeaPackCZ <honza.crha@gmail.com>

--- zmqLogger.py
+++ zmqLogger.py
@@ -5,8 +5,13 @@ A = zMQLogger()
 A.addLocalPort("10101") # GPS1
 A.addLocalPort("10102") # GPS2
 A.addLocalPort("10111") # GPSNav->Main
-A.addLocalPort("10300") # Main->GPSNav
-A.addLocalPort("10302") # test2
+
+A.addLocalPort("10201") # Main->GPSNav & GPS1 & GPS2
+A.addLocalPort("10202") # test2
+
+A.addLocalPort("10300") # Main->Mbed
+A.addLocalPort("10302") # Mbed->Main
+
 A.addIP("192.168.0.1:9000") # RaspiCam?
 
 sleep(1)
